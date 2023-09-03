2264923347316e5ab86394281343c3935514052e
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2023-09-03 11:55:00 +0530

    Create Check if Tree is Isomorphic.cpp

---
+++ Check if Tree is Isomorphic.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(Node *root1, Node *root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        if (root1->data != root2->data) return false;
+
+      
+        bool withoutSwap = (isIsomorphic(root1->left, root2->left) && isIsomorphic(root1->right, root2->right));
+
+
+        bool withSwap = (isIsomorphic(root1->left, root2->right) && isIsomorphic(root1->right, root2->left));
+
+        return withoutSwap || withSwap;
+    }
+};
