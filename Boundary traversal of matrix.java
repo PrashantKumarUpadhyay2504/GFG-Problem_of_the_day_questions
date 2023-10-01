db7abebcb36929bbb1a320a4ae2b56964d2d2da2
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2023-10-01 14:55:12 +0530

    Create Boundary traversal of matrix.java

---
+++ Boundary traversal of matrix.java
@@ -0,0 +1,20 @@
+class Solution
+{
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    static ArrayList<Integer> boundaryTraversal(int matrix[][], int n, int m)
+    {
+        // code here 
+         ArrayList<Integer> ans=new ArrayList<>();
+        
+        for(int i=0;i<m;i++) ans.add(matrix[0][i]);
+        
+        for(int i=1;i<n;i++) ans.add(matrix[i][m-1]);
+        
+        if(n>1) for(int i=m-2;i>=0;i--) ans.add(matrix[n-1][i]);
+        
+        if(m>1) for(int i=n-2;i>=1;i--) ans.add(matrix[i][0]);
+        
+        return ans;
+    }
+}
