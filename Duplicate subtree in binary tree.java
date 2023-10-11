07653aa5c21c5e1637166ab0bfff25129faae9d6
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2023-10-12 00:11:03 +0530

    Create Duplicate subtree in binary tree.java

---
+++ Duplicate subtree in binary tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    int dupSub(Node root) {
+        // code here 
+        if (root == null) return 0;
+        HashMap<String, Integer> hm = new HashMap<>();
+        helper(root, hm);
+        for(HashMap.Entry<String, Integer> entry : hm.entrySet()) {
+            if(entry.getValue() > 1) return 1;
+        }
+        
+        return 0;
+    }
+
+    String helper(Node root, HashMap<String, Integer> hm) {
+        if (root == null) return "#";
+        
+        if(root.left == null && root.right == null) return  String.valueOf(root.data);
+
+        String leftCount = helper(root.left, hm);
+        String rightCount = helper(root.right, hm);
+
+        String subtree = root.data + ", " + leftCount + ", " + rightCount;
+        hm.put(subtree, hm.getOrDefault(subtree, 0) + 1);
+
+        return subtree;
+    }
+}
