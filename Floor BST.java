d2fedc88b31571dbe30be00dba275c5964d58188
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2023-10-13 11:50:47 +0530

    Create Floor BST.java

---
+++ Floor BST.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int floor(Node root, int x) {
+        // Code here
+         int ans = -1;
+        while(root != null){
+            if(x>=root.data){
+                ans = root.data;
+                root = root.right;
+            }else if(x < root.data){
+                root = root.left;
+            }
+        }
+        return ans;
+    }
+}
