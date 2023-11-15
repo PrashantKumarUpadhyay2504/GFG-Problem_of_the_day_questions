d605ca915108e5dd4832bb6a39d961fe70dbfd67
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2023-11-15 19:18:47 +0530

    Create Better Strings.java

---
+++ Better Strings.java
@@ -0,0 +1,22 @@
+int first = countDistrinctSubsequence(str1);
+        int second = countDistrinctSubsequence(str2);
+        if(first>=second){
+            return str1;
+        }else{
+            return str2;
+        }
+    }
+    public static int countDistrinctSubsequence(String s){
+        
+        int dp[] = new int[s.length()+1];
+        dp[0] = 1;
+        
+        int ch[] = new int[26];
+        
+        for(int i=1;i<dp.length;i++){
+            int val = ch[s.charAt(i-1)-'a'];
+            dp[i] = ((2 * dp[i-1])) - val;
+            ch[s.charAt(i-1) - 'a'] = dp[i-1];
+        }
+        int ans = dp[s.length()];
+        return ans;
