1531ff2665148a0703c143b1da1ca81545e2d6b6
Prashant Kumar Upadhyay <78552170+PrashantKumarUpadhyay2504@users.noreply.github.com>
2024-01-17 20:43:24 +0530

    Create All Unique Permutation of an array.cpp

---
+++ All Unique Permutation of an array.cpp
@@ -0,0 +1,15 @@
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        // code here
+         vector<vector<int>>res;
+        sort(arr.begin(),arr.end());
+        do{
+            res.push_back(arr);
+        }
+        while(next_permutation(arr.begin(), arr.end()));
+        return res;
+    }
+};
